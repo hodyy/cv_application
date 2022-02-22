b7e6b0f68208dcabae73a081f1af39403eaa0c19
hodyy <pavel.hodyc@seznam.cz>
2022-02-22 22:13:26 +0100

    commit

--- library/src/main/java/cz/project/cv_aplication/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package cz.project.cv_aplication.repository;
-
-import cz.project.cv_aplication.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository <User, Integer> {
-}
